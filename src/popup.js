4b0beec96525e567e049f3f2d57b5b0b5a285e4f
ishmikler <ivyameyer@gmail.com>
2023-08-09 21:21:32 -0600

    its working

--- src/popup.js
+++ src/popup.js
@@ -1,7 +1,42 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const body = document.querySelector('body')
-    const title = document.createElement('div');
-    title.innerHTML = 'There is text here';
-    body.appendChild(title);
+    const timerDisplay = document.getElementById('timerDisplay');
+    const startButton = document.getElementById('startButton');
+    const resetButton = document.getElementById('resetButton');
+
+    const body = document.querySelector('body');
+    
+    let timer;
+    const time = 60; //Measured in seconds
+    let timeleft = time
+
+    function updateTimerDisplay() {
+        const minutes = Math.floor(timeleft / 60);
+        const seconds = timeleft % 60;
+        timerDisplay.innerHTML = `${minutes}:${seconds.toString().padStart(2, '0')}`
+    }
+
+    startButton.addEventListener('click', function(){
+        if(!timer){
+            timer = setInterval(() => {
+                if(timeleft > 0){
+                    timeleft--
+                    console.log(timeleft)
+                    updateTimerDisplay();
+                } else {
+                    
+                    clearInterval(timer)
+                    timer = null
+                }   
+            }, 1000)
+        }
+     
+    })
+
+    resetButton.addEventListener('click', function (){
+        clearInterval(timer)
+        timeleft = time
+        timer = null
+        updateTimerDisplay();
+    })
 
 });
