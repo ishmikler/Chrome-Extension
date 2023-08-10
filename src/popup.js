901926ff262fe6a8d03685f4ccca4e366b51e3ff
ishmikler <ivyameyer@gmail.com>
2023-08-09 19:29:24 -0600

    basic

---
+++ src/popup.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const body = document.querySelector('body')
+    const title = document.createElement('div');
+    title.innerHTML = 'There is text here';
+    body.appendChild(title);
+
+});
