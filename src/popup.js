4ccecc25f3ee673a1621bcb529f5d5d5c741a9b8
ishmikler <ivyameyer@gmail.com>
2023-08-10 12:34:28 -0600

    music

--- src/popup.js
+++ src/popup.js
@@ -2,17 +2,26 @@ document.addEventListener('DOMContentLoaded', () => {
     const timerDisplay = document.getElementById('timerDisplay');
     const startButton = document.getElementById('startButton');
     const resetButton = document.getElementById('resetButton');
-
+    const audio = new Audio('audio/Soft-alarm-tone.mp3')
     const body = document.querySelector('body');
     
     let timer;
-    const time = 60; //Measured in seconds
+    const time = 10; //Measured in seconds
     let timeleft = time
 
     function updateTimerDisplay() {
         const minutes = Math.floor(timeleft / 60);
         const seconds = timeleft % 60;
         timerDisplay.innerHTML = `${minutes}:${seconds.toString().padStart(2, '0')}`
+    }
+
+    function alarm(){
+        audio.play();
+        audio.volume = .5
+    }
+
+    function stopAlarm(){
+        audio.pause();
     }
 
     startButton.addEventListener('click', function(){
@@ -25,6 +34,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 } else {
                     
                     clearInterval(timer)
+                    alarm()
                     timer = null
                 }   
             }, 1000)
@@ -33,6 +43,7 @@ document.addEventListener('DOMContentLoaded', () => {
     })
 
     resetButton.addEventListener('click', function (){
+        stopAlarm();
         clearInterval(timer)
         timeleft = time
         timer = null
